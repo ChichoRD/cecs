623143c595e5ac0428978ef4e0206acc0393aa1d
ChichoRD <chichord2005@hotmail.com>
2024-08-11 23:23:57 +0200

    nanosecond measurement... 2 ways!

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <windows.h>
+#include <time.h>
 #include "../include/arena.h"
 #include "../include/list.h"
 #include "../include/world.h"
@@ -8,74 +11,68 @@
 #include "../include/query.h"
 #include "../include/resource.h"
 
-COMPONENT_DEFINE(vec2int) {
-    int x;
-    int y;
-} vec2int;
+#define BOARD_WIDTH 64
+#define BOARD_HEIGHT 64
 
-COMPONENT_DEFINE(tag) {
-    char *value;
-} tag;
+typedef enum cell_state {
+    CELL_STATE_ALIVE,
+    CELL_STATE_DEAD
+} cell_state;
 
-COMPONENT_DEFINE(entity_reference) {
-    entity_id id;
-} entity_reference;
+COMPONENT_DEFINE(cell) {
+    uint16_t x;
+    uint16_t y;
+    cell_state state;
+} cell;
 
-RESOURCE_DEFINE(world_time) {
-    double time;
-} world_time;
+RESOURCE_DEFINE(board) {
+    cell cells[BOARD_WIDTH][BOARD_HEIGHT];
+} board;
 
-RESOURCE_DEFINE(input_buffer) {
-    char *buffer[16];
-} input_buffer;
 
-RESOURCE_DEFINE(emerald) {
-    int count;
-} emerald;
+bool Update(double delta_time_seconds) {
+    printf("Delta time: %f\n", delta_time_seconds);
+    return false;
+}
 
 void main(void) {
     world w = world_create();
+
+    bool quitting = false;
+    bool update_error = false;
+
+    struct timespec start, end;
+    timespec_get(&start, TIME_UTC);
+
+    LARGE_INTEGER StartingTime, EndingTime, ElapsedMicroseconds;
+    LARGE_INTEGER Frequency;
+    QueryPerformanceFrequency(&Frequency); 
+    QueryPerformanceCounter(&StartingTime);
+
+    Sleep(1000 / 60);
+    while (!quitting && !update_error)
     {
-        entity_id e = world_add_entity(&w);
-        vec2int *c = WORLD_SET_OR_ADD_COMPONENT(vec2int, w, e, ((vec2int){1, 2}));
-        tag *t = WORLD_SET_OR_ADD_COMPONENT(tag, w, e, ((tag){"Hello"}));
-        entity_reference *er = WORLD_SET_OR_ADD_COMPONENT(entity_reference, w, e, ((entity_reference){e}));
+        timespec_get(&end, TIME_UTC);
 
-        t->value = "Bye";
-    }
+        QueryPerformanceCounter(&EndingTime);
+        ElapsedMicroseconds.QuadPart = EndingTime.QuadPart - StartingTime.QuadPart;
+        ElapsedMicroseconds.QuadPart *= 1000000;
+        ElapsedMicroseconds.QuadPart /= Frequency.QuadPart;
 
-    {
-        world_time *wt = WORLD_ADD_RESOURCE(world_time, w, ((world_time){.time = 0.0}));
-        input_buffer *ib = WORLD_ADD_RESOURCE(input_buffer, w, ((input_buffer){.buffer = {"Up", "Down"}}));
-        emerald *e = WORLD_ADD_RESOURCE(emerald, w, ((emerald){.count = 10}));
+        //double delta_time_seconds = (double)ElapsedMicroseconds.QuadPart / 1000000;
+        double delta_time_seconds = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
 
-        world_time new_wt = WORLD_REMOVE_RESOURCE(world_time, w);
-        input_buffer new_ib = WORLD_REMOVE_RESOURCE(input_buffer, w);
-
-        WORLD_ADD_RESOURCE(input_buffer, w, new_ib);
-    }
-
-    arena a = arena_create();
-    query q = QUERY_CREATE(vec2int, tag, entity_reference);
-    struct QUERY_RESULT(vec2int, tag, entity_reference) *result = QUERY_RUN(q, w, a, vec2int, tag, entity_reference);
-
-    for (size_t i = 0; i < result->vec2int_view.count; i++) {
-        vec2int *v = result->vec2int_view.elements[i];
-        tag *t = result->tag_view.elements[i];
-        entity_reference *er = result->entity_reference_view.elements[i];
-        printf("vec2int: %d, %d, tag: %s, entity: %d\n", v->x, v->y, t->value, er->id);
-
-        world_time *wt = WORLD_GET_RESOURCE(world_time, w);
-        printf("Time: %f\n", wt->time);
-
-        input_buffer *ib = WORLD_GET_RESOURCE(input_buffer, w);
-        for (size_t j = 0; j < 16; j++) {
-            printf("Input: %s\n", ib->buffer[j]);
+        if (Update(delta_time_seconds)) {
+            update_error = true;
         }
-        emerald *e = WORLD_GET_RESOURCE(emerald, w);
-        printf("Emeralds: %d\n", e->count);
+
+        timespec_get(&start, TIME_UTC);
+
+        QueryPerformanceFrequency(&Frequency); 
+        QueryPerformanceCounter(&StartingTime);    
+        Sleep(1000 / 2);
     }
-    arena_free(&a);
+    
     world_free(&w);
     printf("Freed world\n");
 }
