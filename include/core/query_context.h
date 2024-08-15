ec8b64344368805235a3c249dcd9cf6c2bd2857f
ChichoRD <chichord2005@hotmail.com>
2024-08-15 18:35:33 +0200

    cleaned up query_context a bit

--- include/core/query_context.h
+++ include/core/query_context.h
@@ -37,7 +37,7 @@ typedef enum query_search_result {
     QUERY_SEARCH_RESULT_FOUND_SUBMASK,
 } query_search_result;
 
-query_search_result query_context_search_query(query_context *qc, const query q, list **out_query_operations, size_t *out_query_operation_index, query_operation **out_submask_operation) {
+static query_search_result query_context_search_query(query_context *qc, const query q, list **out_query_operations, size_t *out_query_operation_index, query_operation **out_submask_operation) {
     typedef union query_mask_values {
         query_mask mask;
         uint32_t values[(sizeof(component_mask) + sizeof(tag_mask)) / sizeof(uint32_t)];
@@ -50,23 +50,19 @@ query_search_result query_context_search_query(query_context *qc, const query q,
 
     uint32_t tag_count = __popcnt(q.mask.tag_mask);
     size_t rank = tag_count + q.component_count;
-    printf("tag_count: %d, rank: %zu\n", tag_count, rank);
 
     query_mask_values qmv = { .mask = q.mask };
     query_search_result result = QUERY_SEARCH_RESULT_NOT_FOUND;
 
     for (int32_t i = rank; i >= 0; i--) {
-        printf("i: %zu\n", i);
         list *query_operations = &qc->queries_operations[i];
         
         size_t start = 0;
-        size_t end = LIST_COUNT(query_operation, *query_operations);
+        size_t end = LIST_COUNT(query_operation, query_operations);
         size_t mid = 0;
-        printf("query_operations count: %d, capacity: %d\n", query_operations->count, query_operations->capacity);
 
         while (start < end) {
             mid = (start + end) / 2;
-            printf("start: %zu, mid: %zu, end: %zu\n", start, mid, end);
             query_operation *op = LIST_GET(query_operation, query_operations, mid);
             query_mask_values op_qmv = { .mask = op->query };
 
@@ -86,7 +82,7 @@ query_search_result query_context_search_query(query_context *qc, const query q,
         }
 
         if (rank == i) {
-            if (mid >= LIST_COUNT(query_operation, *query_operations)) {
+            if (mid >= LIST_COUNT(query_operation, query_operations)) {
                 result = QUERY_SEARCH_RESULT_NOT_FOUND;
                 *out_query_operations = query_operations;
                 *out_query_operation_index = mid;
@@ -113,7 +109,7 @@ query_search_result query_context_search_query(query_context *qc, const query q,
                     *out_submask_operation = NULL;
                 }
             }
-        } else if (mid < LIST_COUNT(query_operation, *query_operations)) {
+        } else if (mid < LIST_COUNT(query_operation, query_operations)) {
             query_operation *op = LIST_GET(query_operation, query_operations, mid);
             query_mask_values op_qmv = { .mask = op->query };
             bool submask = true;
@@ -126,7 +122,7 @@ query_search_result query_context_search_query(query_context *qc, const query q,
             if (submask) {
                 *out_query_operations = query_operations;
                 *out_query_operation_index = mid;
-                *out_submask_operation = op->result;
+                *out_submask_operation = op;
                 return QUERY_SEARCH_RESULT_FOUND_SUBMASK;
             }
         }
@@ -135,22 +131,24 @@ query_search_result query_context_search_query(query_context *qc, const query q,
     return result;
 }
 
-void *query_context_run_query_on_operation(const query q, const world *w, arena *query_arena, const query_operation operation) {
-    size_t query_results_count = q.component_count + 1;
+static void *query_context_run_query_on_operation(const query q, const world *w, arena *query_arena, const query_operation operation) {
     VIEW_STRUCT(entity_id) *operation_views = (struct VIEW(entity_id) *)operation.result;
-    size_t enitity_count = operation_views->count;
+    size_t entity_count = operation_views->count;
+    size_t initial_capacity = entity_count / 2 + 1;
 
-    list *results = arena_alloc(query_arena, sizeof(list) * query_results_count);
-    results[0] = list_create(query_arena, sizeof(entity_id) * enitity_count);
-    for (size_t i = 0; i < query_results_count; i++) {
-        results[i] = list_create(query_arena, sizeof(intptr_t) * enitity_count);
+    list *entity_results = (list *)arena_alloc(query_arena, sizeof(list) * (q.component_count + 1));
+    *entity_results = list_create(query_arena, sizeof(entity_id) * (initial_capacity));
+
+    list *component_results = entity_results + 1; 
+    for (size_t i = 0; i < q.component_count; i++) {
+        component_results[i] = list_create(query_arena, sizeof(void *) * (initial_capacity));
     }
     
-    for (size_t i = 0; i < enitity_count; i++) {
+    for (size_t i = 0; i < entity_count; i++) {
         entity e = *world_get_entity(w, operation_views->elements[i]);
         if (((e.components & q.mask.component_mask) == q.mask.component_mask)
             && ((e.tags & q.mask.tag_mask) == q.mask.tag_mask)) {
-            list_add(&results[0], query_arena, &e.id, sizeof(entity_id));
+            list_add(entity_results, query_arena, &e.id, sizeof(entity_id));
 
             for (size_t j = 0; j < q.component_count; j++) {
                 void *component = world_components_get_component(
@@ -160,23 +158,27 @@ void *query_context_run_query_on_operation(const query q, const world *w, arena 
                     w->components.component_sizes[q.component_ids[j]]
                 );
 
-                list_add(&results[j + 1], query_arena, &component, sizeof(intptr_t));
+                list_add(&component_results[j], query_arena, &component, sizeof(void *));
             }
         }
     }
 
-    struct VIEW(entity_id) *entity_id_view;
-    VIEW_STRUCT_INDIRECT(void, *) *component_views;
-    entity_id_view = arena_alloc(query_arena, sizeof(struct VIEW(void)) * q.component_count + sizeof(struct VIEW(entity_id)));
-    component_views = (struct VIEW(void) *)(entity_id_view + 1);
+    VIEW_STRUCT_INDIRECT(void, *) *component_views_result;
+    struct VIEW(entity_id) *entity_view_result =
+        arena_alloc(query_arena, sizeof(struct VIEW(entity_id)) + sizeof(struct VIEW(void)) * q.component_count);
+    *entity_view_result = (struct VIEW(entity_id)) {
+        .count = list_count_of_size(entity_results, sizeof(entity_id)),
+        .elements = entity_results->elements,
+    };
 
-    entity_id_view->count = LIST_COUNT_OF_SIZE(results[0], sizeof(entity_id));
-    entity_id_view->elements = results[0].elements;
+    component_views_result = (struct VIEW(void) *)(entity_view_result + 1);
     for (size_t i = 0; i < q.component_count; i++) {
-        component_views[i].count = LIST_COUNT_OF_SIZE(results[i + 1], sizeof(intptr_t));
-        component_views[i].elements = results[i + 1].elements;
+        component_views_result[i] = (struct VIEW(void)) {
+            .count = list_count_of_size(&component_results[i], sizeof(void *)),
+            .elements = component_results[i].elements,
+        };
     }
-    return entity_id_view;
+    return entity_view_result;
 }
 
 void *query_context_run_query(query_context *qc, const query q, const world *w, arena *query_arena) {
@@ -186,32 +188,36 @@ void *query_context_run_query(query_context *qc, const query q, const world *w, 
     switch (query_context_search_query(qc, q, &query_operations, &query_operation_index, &submask_operation))
     {
         case QUERY_SEARCH_RESULT_NOT_FOUND: {
+            printf("QUERY_SEARCH_RESULT_NOT_FOUND\n");
             query_operation op = {
                 .query = q.mask,
                 .result = query_run(q, w, query_arena)
             };
-            LIST_INSERT(query_operation, query_operations, query_arena, query_operation_index, op);
+            LIST_INSERT(query_operation, query_operations, query_arena, query_operation_index, &op);
             return op.result;
         }
 
         case QUERY_SEARCH_RESULT_FOUND: {
+            printf("QUERY_SEARCH_RESULT_FOUND\n");
             query_operation *op = LIST_GET(query_operation, query_operations, query_operation_index);
             return op->result;
         }
 
         case QUERY_SEARCH_RESULT_FOUND_SUBMASK: {
+            printf("QUERY_SEARCH_RESULT_FOUND_SUBMASK\n");
             assert(submask_operation != NULL && "submask_operation is NULL");
             query_operation op = {
                 .query = q.mask,
                 .result = query_context_run_query_on_operation(q, w, query_arena, *submask_operation)
             };
-            LIST_INSERT(query_operation, query_operations, query_arena, query_operation_index, op);
+            LIST_INSERT(query_operation, query_operations, query_arena, query_operation_index, &op);
             return op.result;
         }   
+
         default: {
             assert(0 && "unreachable: query_context_search_query returned invalid value");
             exit(EXIT_FAILURE);
-            //return NULL;
+            return NULL;
         }
     }
 }
