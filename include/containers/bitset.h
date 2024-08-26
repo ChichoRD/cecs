378ac75f05dca17c82f233fe389470771d230040
ChichoRD <chichord2005@hotmail.com>
2024-08-26 21:46:25 +0200

    bitset

--- include/containers/bitset.h
+++ include/containers/bitset.h
@@ -21,244 +21,308 @@ _STATIC_ASSERT(BIT_WORD_BITS == BIT_WORD_BIT_COUNT);
 
 #define BIT_LAYER_COUNT (BIT_WORD_BITS_LOG2 - BIT_PAGE_SIZE_LOG2)
 
-typedef exclusive_range layer_range;
-
-typedef struct bitset {
-    list bit_words[BIT_LAYER_COUNT];
-    layer_range layer_ranges[BIT_LAYER_COUNT];
-} bitset;
-
-inline size_t layer_complement(size_t layer) {
-    return BIT_LAYER_COUNT - layer - 1;
-}
-
-bitset bitset_create(arena *a) {
-    list bit_words[BIT_LAYER_COUNT];
-    for (size_t layer = 0; layer < BIT_LAYER_COUNT; layer++) {
-        bit_words[layer] = list_create(a, sizeof(bit_word) << (BIT_PAGE_SIZE_LOG2 * layer_complement(layer)));
-    }
-    bitset b = {
-        .bit_words = bit_words,
-        .layer_ranges = {0},
-    };
-    return b;
+#define ZERO_PAGE_MASK (((bit_word)1 << BIT_PAGE_SIZE) - 1)
+inline bit_word page_mask(size_t page_index) {
+    return ((bit_word)ZERO_PAGE_MASK << (page_index * BIT_PAGE_SIZE));
 }
 
 inline size_t layer_word_index(size_t bit_index, size_t layer) { 
     return bit_index >> (BIT_WORD_BITS_LOG2 + layer * BIT_PAGE_SIZE_LOG2);
 }
 
+inline size_t bit0_from_layer_word_index(size_t word_index, size_t layer) {
+    return word_index << (BIT_WORD_BITS_LOG2 + layer * BIT_PAGE_SIZE_LOG2);
+}
+
 inline size_t layer_bit_index(size_t bit_index, size_t layer) {
     return bit_index >> (layer * BIT_PAGE_SIZE_LOG2);
+}
+
+inline size_t bit0_from_layer_bit_index(size_t layer_bit_index, size_t layer) {
+    return layer_bit_index << (layer * BIT_PAGE_SIZE_LOG2);
 }
 
 inline size_t layer_word_bit_index(size_t bit_index, size_t layer) {
     return layer_bit_index(bit_index, layer) & (BIT_WORD_BIT_COUNT - 1);
 }
 
-void bit_set_ensure_layer_range(bitset *b, arena *a, layer_range range, size_t layer) {
-    if (range.start < b->layer_ranges[layer].start) {
-        size_t missing_count = b->layer_ranges[layer].start - range.start;
-        bit_word *buffer = calloc(missing_count, sizeof(bit_word));
-        LIST_INSERT_RANGE(bit_word, &b->bit_words[layer], a, 0, buffer, missing_count);
-        free(buffer);
-        b->layer_ranges[layer].start = range.start;
-    } else if (range.end + 1 >= b->layer_ranges[layer].end) {
-        size_t missing_count = range.end + 1 - b->layer_ranges[layer].end;
-        bit_word *buffer = calloc(missing_count, sizeof(bit_word));
-        LIST_ADD_RANGE(bit_word, &b->bit_words[layer], a, buffer, missing_count);
-        free(buffer);
-        b->layer_ranges[layer].end = range.end + 1;
-    }
-}
+typedef exclusive_range word_range;
 
-void bitset_set(bitset *b, arena *a, size_t index) {
-    for (ssize_t layer = BIT_LAYER_COUNT; layer >= 0; layer--) {
-        size_t layer_index = layer_word_index(index, layer);
-        bit_set_ensure_layer_range(b, a, exclusive_range_singleton(layer_index), layer);
-        ssize_t layer_list_index = (ssize_t)layer_index - b->layer_ranges[layer].start;
-        assert(
-            (layer_list_index >= 0)
-            && (layer_list_index < layer_range_length(b->layer_ranges[layer]))
-            && "Index out of bounds"
-        );
-        bit_word *w = LIST_GET(bit_word, &b->bit_words[layer], layer_list_index);
-        *w |= (bit_word)1 << layer_word_bit_index(index, layer);
-    }
-}
+typedef struct bitset {
+    list bit_words;
+    word_range word_range;
+} bitset;
 
-void bitset_unset(bitset *b, arena *a, size_t index) {
-    bool page_unset = true;
-    for (size_t layer = 0; layer < BIT_LAYER_COUNT && page_unset; layer++) {
-        size_t layer_index = layer_word_index(index, layer);
-        bit_set_ensure_layer_range(b, a, exclusive_range_singleton(layer_index), layer);
-        ssize_t layer_list_index = (ssize_t)layer_index - b->layer_ranges[layer].start;
-        assert(
-            (layer_list_index >= 0)
-            && (layer_list_index < layer_range_length(b->layer_ranges[layer]))
-            && "Index out of bounds"
-        );
-        bit_word *w = LIST_GET(bit_word, &b->bit_words[layer], layer_list_index);
-        size_t word_bit_index = layer_word_bit_index(index, layer);
-        *w &= ~(bit_word)1 << word_bit_index;
-
-        size_t page_index = word_bit_index / BIT_PAGE_SIZE;
-        bit_word page = (*w >> (page_index * BIT_PAGE_SIZE)) & ~((bit_word)1 << ((page_index + 1) * BIT_PAGE_SIZE));
-        page_unset = page == 0;
-    }
-}
-
-bool bitset_is_set_skip_check(const bitset *b, size_t index, ssize_t *bit_skip_count) {
-    *bit_skip_count = 1;
-    for (ssize_t layer = BIT_LAYER_COUNT; layer >= 0; layer--) {
-        size_t layer_index = layer_word_index(index, layer);
-        size_t word_bit_index = layer_word_bit_index(index, layer);
-        if (!exclusive_range_contains(b->layer_ranges[layer], layer_index)) {
-            ssize_t word_skip_count = b->layer_ranges[layer].start - layer_index;
-            *bit_skip_count = word_skip_count >= 0 ?
-                word_skip_count << (BIT_WORD_BITS_LOG2 + layer * BIT_PAGE_SIZE_LOG2)              
-                    + (BIT_WORD_BIT_COUNT - word_bit_index - 1) << (BIT_PAGE_SIZE_LOG2 * layer)
-                : -1;
-            return false;
-        }
-        ssize_t layer_list_index = (ssize_t)layer_index - b->layer_ranges[layer].start;
-        bit_word *w = LIST_GET(bit_word, &b->bit_words[layer], layer_list_index);
-        if ((*w & ((bit_word)1 << word_bit_index)) == 0) {
-            *bit_skip_count = (BIT_WORD_BIT_COUNT - word_bit_index - 1) << (BIT_PAGE_SIZE_LOG2 * layer);
-            return false;
-        }
-    }
-    return true;
-}
-
-bool bitset_is_set(const bitset *b, size_t index) {
-    for (ssize_t layer = BIT_LAYER_COUNT; layer >= 0; layer--) {
-        size_t layer_index = layer_word_index(index, layer);
-        if (!exclusive_range_contains(b->layer_ranges[layer], layer_index)) {
-            return false;
-        }
-        ssize_t layer_list_index = (ssize_t)layer_index - b->layer_ranges[layer].start;
-        bit_word *w = LIST_GET(bit_word, &b->bit_words[layer], layer_list_index);
-        size_t word_bit_index = layer_word_bit_index(index, layer);
-        if ((*w & ((bit_word)1 << word_bit_index)) == 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
-
-typedef struct bitset_iterator_descriptor {
-    bitset *bitset;
-} bitset_iterator_descriptor;
-
-typedef struct bitset_iterator {
-    const bitset *const bitset;
-    size_t current_bit;
-} bitset_iterator;
-
-bitset_iterator bitset_iterator_create(const bitset *b) {
-    return (bitset_iterator) {
-        .bitset = b,
-        .current_bit = 0
+bitset bitset_create(arena *a, size_t capacity) {
+    bitset b = (bitset){
+        .bit_words = LIST_CREATE(bit_word, a, capacity),
+        .word_range = {0, 0},
     };
-}
-
-bitset_iterator bitset_iterator_from(bitset_iterator_descriptor descriptor) {
-    return bitset_iterator_create(descriptor.bitset);
-}
-
-bool bitset_iterator_current(const bitset_iterator *iterator) {
-    return bitset_is_set(iterator->bitset, iterator->current_bit);
-}
-
-inline size_t bitset_iterator_next(bitset_iterator *iterator) {
-    return ++iterator->current_bit;
-}
-
-size_t bitset_iterator_next_set(bitset_iterator *iterator) {
-    ssize_t bit_skip = 1;
-    while (!bitset_is_set_skip_check(iterator->bitset, iterator->current_bit, &bit_skip) && bit_skip > 0) {
-        iterator->current_bit += bit_skip;
-    }
-    return iterator->current_bit;
-}
-
-size_t bitset_iterator_next_unset(bitset_iterator *iterator) {
-    ssize_t bit_skip = 1;
-    while (bitset_is_set_skip_check(iterator->bitset, iterator->current_bit, &bit_skip) && bit_skip > 0) {
-        iterator->current_bit += bit_skip;
-    }
-    return iterator->current_bit;
-}
-
-inline bool bitset_iterator_done(const bitset_iterator *iterator) {
-    return iterator->current_bit >= ((iterator->bitset->layer_ranges[0].end - 1) << BIT_WORD_BITS_LOG2);
-}
-
-
-bitset bitset_intersection(VIEW_STRUCT(bitset) bitsets, arena *a) {
-    layer_range layer0_intersection = (layer_range) {
-        .start = bitsets.elements[0].layer_ranges[0].start,
-        .start = bitsets.elements[0].layer_ranges[0].start
-    };
-    for (size_t i = 1; i < bitsets.count; i++) {
-        layer0_intersection = exclusive_range_from(
-            range_intersection(layer0_intersection.range, bitsets.elements[i].layer_ranges[0].range)
-        );
-    }
-    if (exclusive_range_is_empty(layer0_intersection)) {
-        return bitset_create(a);
-    }
-
-    bitset b = bitset_create(a);
-    bitset_iterator it = bitset_iterator_create(&bitsets.elements[0]);
-    it.current_bit = layer0_intersection.start << BIT_WORD_BITS_LOG2;
-
-    while (!bitset_iterator_done(&it)) {
-        bool all_set = true;
-        for (size_t i = 0; i < bitsets.count && all_set; i++) {
-            all_set &= bitset_is_set(&bitsets.elements[i], it.current_bit);
-        }
-        if (all_set) {
-            bitset_set(&b, a, it.current_bit);
-        }
-        bitset_iterator_next_set(&it);
-    }
-
+    //bit_word *buffer = calloc(capacity, sizeof(bit_word));
+    //LIST_ADD_RANGE(bit_word, &b.bit_words, a, buffer, capacity);
+    //free(buffer);
     return b;
 }
 
-bitset bitset_union(VIEW_STRUCT(bitset) bitsets, arena *a) {
-    layer_range layer0_union = (layer_range) {
-        .start = bitsets.elements[0].layer_ranges[0].start,
-        .start = bitsets.elements[0].layer_ranges[0].start
+word_range bitset_expand(bitset *b, arena *a, size_t word_index) {
+    word_range expanded_range = exclusive_range_from(
+        range_union(b->word_range.range, exclusive_range_singleton(word_index).range)
+    );
+    range_splitting expansion_ranges = range_difference(
+        expanded_range.range,
+        b->word_range.range
+    );
+
+    word_range range0 = exclusive_range_from(expansion_ranges.ranges[0]);
+    word_range range1 = exclusive_range_from(expansion_ranges.ranges[1]);
+    if (!exclusive_range_is_empty(range0)) {
+        size_t missing_count = exclusive_range_length(range0);
+        bit_word *buffer = calloc(missing_count, sizeof(bit_word));
+        LIST_INSERT_RANGE(bit_word, &b->bit_words, a, 0, buffer, missing_count);
+        free(buffer);
+    } else if (!exclusive_range_is_empty(range1)) {
+        size_t missing_count = exclusive_range_length(range1);
+        bit_word *buffer = calloc(missing_count, sizeof(bit_word));
+        LIST_ADD_RANGE(bit_word, &b->bit_words, a, buffer, missing_count);
+        free(buffer);
+    }
+
+    return (b->word_range = expanded_range);
+}
+
+bit_word bitset_set(bitset *b, arena *a, size_t bit_index) {
+    size_t word_index = layer_word_index(bit_index, 0);
+    if (!exclusive_range_contains(b->word_range, word_index)) {
+        bitset_expand(b, a, word_index);
+    }
+    ssize_t list_index = (ssize_t)word_index - b->word_range.start;
+    assert(
+        list_index >= 0
+        && list_index < exclusive_range_length(b->word_range)
+        && "Index out of bounds, should have expanded or expansion failed"
+    );
+    bit_word *w = LIST_GET(bit_word, &b->bit_words, list_index);
+    return (*w |= (bit_word)1 << layer_word_bit_index(bit_index, 0));
+}
+
+bit_word bitset_unset(bitset *b, arena *a, size_t bit_index) {
+    size_t word_index = layer_word_index(bit_index, 0);
+    if (!exclusive_range_contains(b->word_range, word_index)) {
+        bitset_expand(b, a, word_index);
+    }
+    ssize_t list_index = (ssize_t)word_index - b->word_range.start;
+    assert(
+        list_index >= 0
+        && list_index < exclusive_range_length(b->word_range)
+        && "Index out of bounds, should have expanded or expansion failed"
+    );
+    bit_word *w = LIST_GET(bit_word, &b->bit_words, list_index);
+    return (*w &= ~((bit_word)1 << layer_word_bit_index(bit_index, 0)));
+}
+
+bool bitset_is_set(bitset *b, size_t bit_index) {
+    size_t word_index = layer_word_index(bit_index, 0);
+    if (!exclusive_range_contains(b->word_range, word_index)) {
+        return false;
+    }
+    ssize_t list_index = (ssize_t)word_index - b->word_range.start;
+    bit_word *w = LIST_GET(bit_word, &b->bit_words, list_index);
+    return (*w & ((bit_word)1 << layer_word_bit_index(bit_index, 0))) != 0;
+}
+
+bit_word bitset_get_word(bitset *b, size_t bit_index) {
+    size_t word_index = layer_word_index(bit_index, 0);
+    if (!exclusive_range_contains(b->word_range, word_index)) {
+        return (bit_word)0;
+    }
+    ssize_t list_index = (ssize_t)word_index - b->word_range.start;
+    bit_word *w = LIST_GET(bit_word, &b->bit_words, list_index);
+    return *w;
+}
+
+
+typedef struct bitset_iterator {
+    const bitset *const bitset;
+    size_t current_bit_index;
+} bitset_iterator;
+
+bitset_iterator bitset_iterator_create(const bitset *b) {
+    return (bitset_iterator){
+        .bitset = b,
+        .current_bit_index = bit0_from_layer_word_index(b->word_range.start, 0),
     };
+};
 
-    for (size_t i = 1; i < bitsets.count; i++) {
-        layer0_union = exclusive_range_from(
-            range_union(layer0_union.range, bitsets.elements[i].layer_ranges[0].range)
-        );
+inline bool bitset_iterator_done(const bitset_iterator *it) {
+    return layer_word_index(it->current_bit_index, 0) >= it->bitset->word_range.end;
+}
+
+inline size_t bitset_iterator_next(bitset_iterator *it) {
+    return ++it->current_bit_index;
+}
+
+size_t bitset_iterator_next_set(bitset_iterator *it) {
+    do {
+        bitset_iterator_next(it);
+    } while (!bitset_iterator_done(it) && !bitset_is_set(it->bitset, it->current_bit_index));
+    return it->current_bit_index;
+}
+
+size_t bitset_iterator_next_unset(bitset_iterator *it) {
+    do {
+        bitset_iterator_next(it);
+    } while (!bitset_iterator_done(it) && bitset_is_set(it->bitset, it->current_bit_index));
+    return it->current_bit_index;
+}
+
+inline size_t bitset_iterator_current(const bitset_iterator *it) {
+    return it->current_bit_index;
+}
+
+inline bool bitset_iterator_current_is_set(const bitset_iterator *it) {
+    return bitset_is_set(it->bitset, bitset_iterator_current(it));
+}
+
+
+typedef struct hibitset {
+    bitset bitsets[BIT_LAYER_COUNT];
+} hibitset;
+
+inline size_t layer_complement(size_t layer) {
+    return BIT_LAYER_COUNT - layer - 1;
+}
+
+hibitset hibitset_create(arena *a) {
+    hibitset b;
+    for (size_t layer = 0; layer < BIT_LAYER_COUNT; layer++) {
+        b.bitsets[layer] = bitset_create(a, (size_t)1 << (BIT_PAGE_SIZE_LOG2 * layer_complement(layer)));
     }
-    if (exclusive_range_is_empty(layer0_union)) {
-        return bitset_create(a);
+    return b;
+}
+
+void hibitset_set(hibitset *b, arena *a, size_t bit_index) {
+    for (ssize_t layer = BIT_LAYER_COUNT - 1; layer >= 0; layer--) {
+        size_t layer_bit = layer_bit_index(bit_index, layer);
+        bitset_set(&b->bitsets[layer], a, layer_bit);
     }
+}
 
-    bitset b = bitset_create(a);
-    bitset_iterator it = bitset_iterator_create(&bitsets.elements[0]);
-    it.current_bit = layer0_union.start << BIT_WORD_BITS_LOG2;
+void hibitset_unset(hibitset *b, arena *a, size_t bit_index) {
+    for (size_t layer = 0; layer < BIT_LAYER_COUNT; layer++) {
+        size_t layer_bit = layer_bit_index(bit_index, layer);
+        bit_word unset_word = bitset_unset(&b->bitsets[layer], a, layer_bit);
 
-    while (!bitset_iterator_done(&it)) {
+        size_t layer_word_bit_page = layer_word_bit_index(bit_index, layer) / BIT_PAGE_SIZE;
+        bit_word unset_word_page = unset_word & page_mask(layer_word_bit_page);
+
+        if (unset_word_page != 0) {
+            break;
+        }
+    }
+}
+
+bool hibitset_is_set(const hibitset *b, size_t bit_index) {
+    return bitset_is_set(&b->bitsets[0], bit_index);
+}
+
+bool hibitset_is_set_skip_unset(const hibitset *b, size_t bit_index, ssize_t *out_unset_bit_skip_count) {
+    *out_unset_bit_skip_count = 1;
+    for (ssize_t layer = BIT_LAYER_COUNT - 1; layer >= 0; layer--) {
+        size_t layer_bit = layer_bit_index(bit_index, layer);
+        if (!bitset_is_set(&b->bitsets[layer], layer_bit)) {
+            *out_unset_bit_skip_count =
+                bit0_from_layer_bit_index(layer_bit + 1, layer) - bit_index;
+            assert(*out_unset_bit_skip_count > 0);
+            return false;
+        }
+    }
+    return true;
+}
+
+bit_word hibitset_get_word(const hibitset *b, size_t bit_index) {
+    return bitset_get_word(&b->bitsets[0], bit_index);
+}
+
+
+typedef struct hibitset_iterator {
+    const hibitset *const hibitset;
+    size_t current_bit_index;
+} hibitset_iterator;
+
+hibitset_iterator hibitset_iterator_create(const hibitset *b) {
+    return (hibitset_iterator){
+        .hibitset = b,
+        .current_bit_index = bit0_from_layer_word_index(b->bitsets[0].word_range.start, 0),
+    };
+};
+
+inline bool hibitset_iterator_done(const hibitset_iterator *it) {
+    return layer_word_index(it->current_bit_index, 0) >= it->hibitset->bitsets[0].word_range.end;
+}
+
+inline size_t hibitset_iterator_next(hibitset_iterator *it) {
+    return ++it->current_bit_index;
+}
+
+size_t hibitset_iterator_next_set(hibitset_iterator *it) {
+    ssize_t unset_bit_skip_count = 1;
+    do {
+        it->current_bit_index += unset_bit_skip_count;
+    } while (!hibitset_iterator_done(it)
+        && !hibitset_is_set_skip_unset(it->hibitset, it->current_bit_index, &unset_bit_skip_count));
+    return it->current_bit_index;
+}
+
+inline size_t hibitset_iterator_current(const hibitset_iterator *it) {
+    return it->current_bit_index;
+}
+
+inline bool hibitset_iterator_current_is_set(const hibitset_iterator *it) {
+    return hibitset_is_set(it->hibitset, hibitset_iterator_current(it));
+}
+
+
+hibitset hibitset_intersection(hibitset *bitsets, size_t count, arena *a) {
+    hibitset b = hibitset_create(a);
+    hibitset_iterator it = hibitset_iterator_create(&bitsets[0]);
+    while (!hibitset_iterator_done(&it)) {
+        bool all_set = true;
+        size_t max_next_bit = hibitset_iterator_current(&it);
+        for (size_t i = 0; i < count; i++) {
+            all_set &= hibitset_is_set(&bitsets[i], hibitset_iterator_current(&it));
+
+            hibitset_iterator j = hibitset_iterator_create(&bitsets[i]);
+            j.current_bit_index = it.current_bit_index;
+            size_t next_bit = hibitset_iterator_next_set(&j);
+            max_next_bit = max(max_next_bit, next_bit);
+        }
+        if (all_set) {
+            hibitset_set(&b, a, hibitset_iterator_current(&it));
+        }
+        it.current_bit_index = max_next_bit;
+    }
+    return b;
+}
+
+hibitset hibitset_union(hibitset *bitsets, size_t count, arena *a) {
+    hibitset b = hibitset_create(a);
+    hibitset_iterator it = hibitset_iterator_create(&bitsets[0]);
+    while (!hibitset_iterator_done(&it)) {
         bool any_set = false;
-        for (size_t i = 0; i < bitsets.count && !any_set; i++) {
-            any_set |= bitset_is_set(&bitsets.elements[i], it.current_bit);
+        size_t min_next_bit = SIZE_MAX;
+        for (size_t i = 0; i < count; i++) {
+            any_set |= hibitset_is_set(&bitsets[i], hibitset_iterator_current(&it));
+
+            hibitset_iterator j = hibitset_iterator_create(&bitsets[i]);
+            j.current_bit_index = it.current_bit_index;
+            size_t next_bit = hibitset_iterator_next_set(&j);
+            min_next_bit = min(min_next_bit, next_bit);
         }
         if (any_set) {
-            bitset_set(&b, a, it.current_bit);
+            hibitset_set(&b, a, hibitset_iterator_current(&it));
         }
-        bitset_iterator_next_set(&it);
+        it.current_bit_index = min_next_bit;
     }
-
     return b;
 }
 
