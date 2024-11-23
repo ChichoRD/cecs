f9aab1c1115252cd0a0a269a82dbd9e813160bdf
ChichoRD <chichord2005@hotmail.com>
2024-11-23 01:23:42 +0100

    feat: got adapter, logging properties

--- examples/graphics_app/src/main.c
+++ examples/graphics_app/src/main.c
@@ -1,8 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include <webgpu/webgpu.h>
+#include <webgpu/wgpu.h>
 #include <GLFW/glfw3.h>
+
+struct adpater_request_userdata {
+    bool adapter_request_completed;
+    WGPUAdapter adapter;
+};
+
+void on_adapter_request_completed(
+    WGPURequestAdapterStatus status,
+    WGPUAdapter adapter,
+    const char *message,
+    void *userdata
+) {
+    struct adpater_request_userdata *adapter_request_userdata = userdata;
+    if (status == WGPURequestAdapterStatus_Success) {
+        adapter_request_userdata->adapter = adapter;
+        adapter_request_userdata->adapter_request_completed = true;
+        printf("Got WebGPU adapter: %p\n", adapter);
+    } else {
+        adapter_request_userdata->adapter_request_completed = true;
+        adapter_request_userdata->adapter = NULL;
+        printf("Could not get WebGPU adapter: %s\n", message);
+    }
+}
+
+WGPUAdapter request_adapter_sync(
+    WGPUInstance instance,
+    const WGPURequestAdapterOptions *options
+) {
+    struct adpater_request_userdata userdata = {
+        .adapter_request_completed = false,
+        .adapter = NULL
+    };
+    wgpuInstanceRequestAdapter(
+        instance,
+        options,
+        on_adapter_request_completed,
+        &userdata
+    );
+    while (!userdata.adapter_request_completed) { }
+    return userdata.adapter;
+}
 
 int main(void) {
     if (!glfwInit()) {
@@ -19,7 +62,11 @@ int main(void) {
         return EXIT_FAILURE;
     }
 
-
+    WGPUInstanceExtras instance_extras = {
+        .chain = { .next = NULL, .sType = WGPUSType_InstanceExtras },
+        .backends = WGPUBackendType_Vulkan,
+    };
+    // unused: instance_extras
     WGPUInstanceDescriptor instance_descriptor = {
         .nextInChain = NULL,
     };
@@ -31,14 +78,57 @@ int main(void) {
     printf("WGPU Instance: %p\n", instance);
 
 
+    WGPURequestAdapterOptions adapter_options = {
+        .nextInChain = NULL,
+        .powerPreference = WGPUPowerPreference_HighPerformance
+    };
+    WGPUAdapter adapter = request_adapter_sync(instance, &adapter_options);
+    WGPUSupportedLimits supported_limits = { .nextInChain = NULL };
+
+    bool adapter_limits_success = false;
+#ifdef WEBGPU_BACKEND_DAWN
+    adapter_limits_success = wgpuAdapterGetLimits(adapter, &supported_limits) == WGPUStatus_Success;
+#else 
+    adapter_limits_success = wgpuAdapterGetLimits(adapter, &supported_limits);
+#endif
+
+    WGPUFeatureName *features;
+    size_t feature_count = wgpuAdapterEnumerateFeatures(adapter, NULL);
+    features = calloc(feature_count, sizeof(WGPUFeatureName));
+    wgpuAdapterEnumerateFeatures(adapter, features);
+
+    printf("Adapter features: \n");
+    for (size_t i = 0; i < feature_count; i++) {
+        printf("\t- 0x%X\n", features[i]);
+    }
+
+    WGPUAdapterProperties adapter_properties = { .nextInChain = NULL };
+    wgpuAdapterGetProperties(adapter, &adapter_properties);
+
+    printf("Adapter properties: \n");
+    printf("\t- Device ID: %d\n", adapter_properties.deviceID);
+
+    printf("\t- Vendor ID: %d\n", adapter_properties.vendorID);
+    printf("\t- Vendor name: %s\n", adapter_properties.vendorName);
+
+    printf("\t- Adapter name: %s\n", adapter_properties.name);
+
+    printf("\t- Backend type: 0x%X\n", adapter_properties.backendType);
+    printf("\t- Adapter type: 0x%X\n", adapter_properties.adapterType);
+    printf("\t- Driver description: %s\n", adapter_properties.driverDescription);
+
+    // TODO: move creation and dbg logging to a separate function
+
     while (!glfwWindowShouldClose(window)) {
         // Check whether the user clicked on the close button (and any other
         // mouse/key event, which we don't use so far)
         glfwPollEvents();
     }
 
-    glfwDestroyWindow(window);
+    free(features);
+    wgpuAdapterRelease(adapter);
     wgpuInstanceRelease(instance);
+    glfwDestroyWindow(window);
     glfwTerminate();
     return EXIT_SUCCESS;
 }
